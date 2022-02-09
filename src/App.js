ace77b09e905d57adfceddac809cdb806e592b98
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-02-08 22:37:59 -0800

    Create our story section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeaderSection from './components/header-section';
 import NavbarBase from './components/Navbar/navbar-base';
+import OurStory from './components/our-story/our-story';
 
 function App() {
   return (
     <div className="App">
       <NavbarBase></NavbarBase>
       <HeaderSection></HeaderSection>
+      <OurStory></OurStory>
     </div>
   );
 }
