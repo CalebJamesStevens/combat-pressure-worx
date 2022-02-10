331aec250ec11579e9d3c4531ed97a68d6309c27
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-02-09 21:27:31 -0800

    Implemented work image slider

--- src/components/our-work/work-image-carousel.js
+++ src/components/our-work/work-image-carousel.js
@@ -5,7 +5,7 @@ import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slide
 function WorkImageCarousel() {
     const after1 = require('../../before-and-after-pics/after1.jpeg') 
     const before1 = require('../../before-and-after-pics/before1.jpeg') 
-    const [workPics, setWorkPics] = useState(new Array())
+    const [workPics, setWorkPics] = useState()
     const [currentPicSet, setCurrentPicSet] = useState(0)
     
     useEffect(() => {
@@ -15,18 +15,29 @@ function WorkImageCarousel() {
     }, [])
 
 
+    const handleCarouselClick = (num) => {
+        if (currentPicSet + num < 0 || currentPicSet + num + 1 > workPics?.length) {
+            console.log('Limit exceeded')
+            return;
+        }
+
+        setCurrentPicSet(current => current + num)
+    }
+
     return (
         <>
         <div className="work-image-carousel-container">
-            <FaChevronLeft  className='carousel-sliders react-icon clickable' style={{'--icon-size': '4'}}></FaChevronLeft>
+            <FaChevronLeft  onClick={() => {handleCarouselClick(-1)}} className='carousel-sliders react-icon clickable' style={{'--icon-size': '4'}}></FaChevronLeft>
             <div className="work-images-container">
+                {workPics && (
                 <ReactCompareSlider
                     className='work-after-pic'
-                    itemOne={<img className='work-after-pic' src={workPics[currentPicSet][0]}/>}
-                    itemTwo={<img className='work-after-pic' src={workPics[currentPicSet][1]}/>}
+                    itemOne={<img className='work-pic' src={workPics[currentPicSet][0]}/>}
+                    itemTwo={<img className='work-pic' src={workPics[currentPicSet][1]}/>}
                 />
+                )}
             </div>
-            <FaChevronRight  className='carousel-sliders react-icon clickable' style={{'--icon-size': '4'}}></FaChevronRight>
+            <FaChevronRight  onClick={() => {handleCarouselClick(1)}} className='carousel-sliders react-icon clickable' style={{'--icon-size': '4'}}></FaChevronRight>
         </div>
         </>
     )
