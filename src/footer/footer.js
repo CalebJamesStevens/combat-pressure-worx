e1881a224cd3b126d71fedccfcb0796940fa9d4c
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-02-09 22:11:49 -0800

    Made foor responsive

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -5,8 +5,8 @@ function Footer() {
         <>
         <div className="footer-container">
             <div className="footer-contacts">
-                <div className="footer-contact-item"><a id="tel-link" href="tel:503-327-1804">503-327-1804</a></div>
                 <div className="footer-contact-item"><FaFacebookSquare/></div>
+                <div className="footer-contact-item"><a id="tel-link" href="tel:503-327-1804">503-327-1804</a></div>
                 <div className="footer-contact-item"><a id="tel-link" href="mailto:Troutbum16@gmail.com">Troutbum16@gmail.com</a></div>
             </div>
             <div className="footer-menu">
